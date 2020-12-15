28d66af2cd2c49076b70c2161826c3b21ea24511
Milindkumar Ukey <68543868+milindukey@users.noreply.github.com>
2020-12-15 23:48:08 +0530

    loops solution

---
+++ Hackerrank solutions/Loops.py
@@ -0,0 +1,14 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # The provided code stub reads and integer, , from STDIN. For all non-negative integers i<n, print its square.
+>>> # constraints: 1<=n<=20
+>>> 
+>>> if __name__ == '__main__':
+	n = int(input())
+	if 1<=n<=20:
+		for i in range(n):
+        print(i**2)
+[DEBUG ON]
+>>> 
+[DEBUG OFF]
+>>> 
